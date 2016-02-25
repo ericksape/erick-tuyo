5164552c7484a5654a59da4113204ad5c048b5ed
erick flores <law_erick@hotmail.com>
2016-02-25 15:58:41 -0500

    terminado

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CodeYork</title>
+</head>
+<body>
+
+<nav>
+	<ul>
+	    
+		<li><a target="_blank" href="https://www.facebook.com/">Facebook</a></li>
+		<li><a target="_blank" href="https://www.youtube.com/">YouTube</a></li>
+		<li><a target="_blank" href="https://www.google.com.pe/">Google</a></li>
+		<li><a target="_blank" href="http://animeflv.net/anime/one-piece.html">AnimeFLV</a></li>
+		</marquee>
+	</ul>
+</nav>
+
+<center>
+
+	<img src="facebook.gif" width="300">
+	<img src="youtube.gif" width="330">
+	<img src="google.gif" width="280">
+	<img src="animeflv.gif" width="320">
+	
+</center>
+<style type="text/css">
+	ul{
+		list-style-type: none;
+	}
+	a{
+		text-decoration: none;
+	}
+	li{
+		display: inline-block;
+		background-color: black;
+		padding: 45px;
+		border-radius: 20px;
+		}
+		a{
+			font-size: 35px;
+			color: red;
+			margin: 20px;
+			padding: 15px;
+
+		}
+</style>
+
+</body>
+</html>
